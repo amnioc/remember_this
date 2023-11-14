72beb0b066db8fd3a1b1d8dbb3834d3e7bb4c45d
amnioc <nicdhillon19@gmail.com>
2023-11-14 16:08:13 +0000

    created homepage and notesPage

--- remember/App.js
+++ remember/App.js
@@ -1,12 +1,7 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View, Button, ScrollView } from "react-native";
 import db from "./firebaseConfig copy";
-import Header from "./components/header/Header";
-import NewNote from "./features/newNote/NewNote";
 import React from "react";
-import { doc, setDoc } from "firebase/firestore";
-import NotesList from "./features/notesList/ShowNotesButton";
-import ShowNotesButton from "./features/notesList/ShowNotesButton";
+import HomePage from "./views/homepage/HomePage";
+import NotesPage from "./views/viewNotes/NotesPage";
 
 export default function App() {
   const [currentNotes, setCurrentNotes] = React.useState([]);
@@ -15,83 +10,27 @@ export default function App() {
 
   const [categories, setCategories] = React.useState([]);
 
-  async function writeNoteData(name, noteId, categories) {
-    const newNote = doc(db, "notes", noteId);
-    const data = {
-      id: noteId,
-      content: name,
-      category: categories,
-      date: `${new Date()}`,
-    };
-    await setDoc(newNote, data)
-      .then(() => {
-        console.log("new note in database");
-        setText("");
-        setCategories([]);
-      })
-      .catch((error) => {
-        console.log(`there was an error! ${error}`);
-      });
-  }
-
   if (showNotes === false) {
     return (
-      <View style={styles.homepage}>
-        <Header style={styles.header} />
-        <NewNote
-          text={text}
-          setText={setText}
-          categories={categories}
-          setCategories={setCategories}
-        />
-        <Button
-          title="Save Note"
-          color={"purple"}
-          onPress={() => {
-            writeNoteData(`${text}`, `${new Date().getTime()}`, categories);
-          }}
-          accessibilityLabel="Save your Note using this purple button"
-        />
-        <ShowNotesButton
-          setShowNotes={setShowNotes}
-          showNotes={showNotes}
-          currentNotes={currentNotes}
-          setCurrentNotes={setCurrentNotes}
-        />
-
-        <StatusBar style="auto" />
-      </View>
+      <HomePage
+        text={text}
+        setText={setText}
+        categories={categories}
+        setCategories={setCategories}
+        showNotes={showNotes}
+        setShowNotes={setShowNotes}
+        currentNotes={currentNotes}
+        setCurrentNotes={setCurrentNotes}
+      />
     );
   } else {
     return (
-      <View style={styles.homepage}>
-        <Header style={styles.header} />
-
-        <ShowNotesButton
-          setShowNotes={setShowNotes}
-          showNotes={showNotes}
-          currentNotes={currentNotes}
-          setCurrentNotes={setCurrentNotes}
-        />
-
-        <StatusBar style="auto" />
-      </View>
+      <NotesPage
+        showNotes={showNotes}
+        setShowNotes={setShowNotes}
+        currentNotes={currentNotes}
+        setCurrentNotes={setCurrentNotes}
+      />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  homepage: {
-    flex: 1,
-    backgroundColor: "#ECECEC",
-    alignItems: "center",
-    justifyContent: "space-evenly",
-    borderColor: "pink",
-  },
-  header: {
-    height: 55,
-    alignItems: "center",
-    backgroundColor: "purple",
-    flexDirection: "row",
-  },
-});
