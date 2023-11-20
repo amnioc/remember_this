52e09a28df2ae11ea4a39efb13b34cda27c14d9f
amnioc <nicdhillon19@gmail.com>
2023-11-20 16:39:41 +0000

    adjusted styling of Notes Page and pressable button

--- remember/views/NotesPage/NotesPage.jsx
+++ remember/views/NotesPage/NotesPage.jsx
@@ -1,12 +1,12 @@
 import { StatusBar } from "expo-status-bar";
-import { ScrollView, StyleSheet, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import ShowNotesButton from "../../features/notesList/ShowNotesButton";
 import Header from "../../components/header/Header";
 
 const NotesPage = ({ showNotes, setShowNotes, notes, setNotes }) => {
   return (
-    <ScrollView style={styles.notesPage}>
-      <Header style={styles.header} />
+    <View style={styles.notesPage}>
+      <Header />
 
       <ShowNotesButton
         setShowNotes={setShowNotes}
@@ -16,19 +16,17 @@ const NotesPage = ({ showNotes, setShowNotes, notes, setNotes }) => {
       />
 
       <StatusBar style="auto" />
-    </ScrollView>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   notesPage: {
+    paddingTop: 50,
     flex: 1,
     backgroundColor: "#ECECEC",
-  },
-  header: {
-    height: 55,
     alignItems: "center",
-    backgroundColor: "purple",
+    justifyContent: "space-between",
   },
 });
 export default NotesPage;
