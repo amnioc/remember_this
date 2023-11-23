b3803fddec36f0c55409d70958bf4d22fab861f2
amnioc <nicdhillon19@gmail.com>
2023-11-23 14:03:05 +0000

    mutation tests for optimisticDelete helper

--- remember/helpers/__tests__/helpers.test.js
+++ remember/helpers/__tests__/helpers.test.js
@@ -3,6 +3,12 @@ import { optimisticDelete } from "../helperFuncs";
 //unit tests of helper functions
 
 describe("optimisticDelete", () => {
+  const testNotes = [
+    { note: 1, id: 1 },
+    { note: 2, id: 2 },
+    { note: 3, id: 3 },
+  ];
+
   test("should return error if no input provided", () => {
     const result = optimisticDelete();
     expect(typeof result).toBe("string");
@@ -14,16 +20,34 @@ describe("optimisticDelete", () => {
     expect(Array.isArray(result)).toBe(true);
   });
 
-  test("returns notes array of unchanged length with invalid ID", () => {
-    const testNotes = [
+  test("returns notes array of unchanged length and new memory when invalid notesID", () => {
+    const testID = 4;
+    const result = optimisticDelete(testNotes, testID);
+    expect(result.length).toBe(testNotes.length);
+    expect(result).not.toBe(testNotes);
+  });
+
+  test("should return an array without note object with matching noteID ", () => {
+    const testID = 2;
+    const expected = [
+      { note: 1, id: 1 },
+      { note: 3, id: 3 },
+    ];
+    const result = optimisticDelete(testNotes, testID);
+    expect(result).toEqual(expected);
+  });
+
+  test("does not mutate each note object", () => {
+    const testNewNotes = [
       { note: 1, id: 1 },
       { note: 2, id: 2 },
       { note: 3, id: 3 },
     ];
-    const testID = 4;
-    const result = optimisticDelete(testNotes, testID);
-    expect(result.length).toEqual(testNotes.length);
+
+    optimisticDelete(testNotes, 4);
+
+    testNotes.forEach((note, index) => {
+      expect(note).toEqual(testNewNotes[index]);
+    });
   });
-  // should return array of unchanged length if ID not found
-  // should return array without ID
 });
