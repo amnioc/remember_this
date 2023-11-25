7962a907f6b55783c7c160846ef0359f82b10026
amnioc <nicdhillon19@gmail.com>
2023-11-25 16:44:48 +0000

    create FlatList for search notes by Category

--- remember/features/searchNotes/SearchNotes.jsx
+++ remember/features/searchNotes/SearchNotes.jsx
@@ -1,10 +1,42 @@
-import { View, Text, SafeAreaView, StyleSheet } from "react-native";
+import React from "react";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  Pressable,
+  FlatList,
+} from "react-native";
+import starterCategories2 from "../../helpers/holders";
 
 const SearchNotes = ({ notes, setNotes }) => {
+  const [filter, setFilter] = React.useState({
+    category: [],
+    date: 0,
+    text: "",
+  });
+
+  function filterResults() {
+    return console.log("in here");
+  }
+
   return (
     <SafeAreaView style={styles.searchMenu}>
       <Text>SEARCH NOTES BY:</Text>
-      <Pressable>Category</Pressable>
+      <FlatList
+        data={starterCategories2}
+        renderItem={({ item }) => (
+          <Pressable
+            style={styles.categoryFilter}
+            onPress={() => {
+              filterResults();
+            }}
+          >
+            <Text style={styles.filterLabel}>{item.category}</Text>
+          </Pressable>
+        )}
+        keyExtractor={(item) => item.id}
+      />
     </SafeAreaView>
   );
 };
@@ -16,6 +48,16 @@ const styles = StyleSheet.create({
     marginTop: 50,
     padding: 20,
     width: "100%",
+  },
+  categoryFilter: {
+    backgroundColor: "grey",
+    width: "25%",
+    padding: 10,
+    margin: 10,
+  },
+  filterLabel: {
+    color: "white",
+    textAlign: "center",
   },
 });
 export default SearchNotes;
---
+++ remember/helpers/holders.js
@@ -0,0 +1,9 @@
+const starterCategories2 = [
+  { id: 1, category: "Work" },
+  { id: 2, category: "Hobbies" },
+  { id: 3, category: "Holiday" },
+  { id: 4, category: "Food" },
+  { id: 5, category: "Link" },
+];
+
+export default starterCategories2;
